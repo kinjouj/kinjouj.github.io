beb3f0701cfe3598d60fe71dde6e12fbc7e16082
kinjouj <kinjouj+github@gmail.com>
2018-07-18 15:50:10 +0900

    Site updated at 2018-07-18 06:50:10 UTC

--- sw.js
+++ sw.js
@@ -88,5 +88,3 @@ self.__precacheManifest = [
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(/.html$/, workbox.strategies.cacheFirst(), 'GET');
