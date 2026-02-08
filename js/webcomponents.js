c5ede5320f978f7712389c9241b9e6b2450e18f3
kinjouj <kinjouj@gmail.com>
2026-02-08 18:17:05 +0900

    Site updated at 2026-02-08 09:17:05 UTC

---
+++ js/webcomponents.js
@@ -0,0 +1,45 @@
+class SearchModal extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="modal fade" id="searchModal">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content bg-transparent">
+            <div class="modal-body">
+              <form action="https://www.google.com/search" method="GET" onsubmit="bootstrap.Modal.getInstance(document.getElementById('searchModal')).hide()">
+                <div class="input-group">
+                  <input type="hidden" name="hl" value="ja" />
+                  <input type="hidden" name="sitesearch" value="kinjouj.github.io" />
+                  <input type="search" name="q" class="form-control py-4 rounded-2" placeholder="Search" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    const modal = document.getElementById('searchModal');
+    modal.addEventListener('shown.bs.modal', () => {
+      requestAnimationFrame(() => {
+        document.querySelector('#searchModal input[name="q"]').focus();
+      });
+    });
+
+    modal.addEventListener('hide.bs.modal', () => {
+      $('.modal').removeAttr('aria-hidden');
+    });
+  }
+}
+
+class BackToTopButton extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <a href="#top" class="btn btn-info btn-floating btn-lg btn-back-to-top">
+        <i class="fas fa-arrow-up"></i>
+      </a>
+    `;
+  }
+}
+
+customElements.define('search-modal', SearchModal);
+customElements.define('back-to-top', BackToTopButton);
